7a415bf1ff2f3630926eafa1088959ce217c6d11
Marcos Hernan Bracciale Sauro <marcoshernanbracciale@gmail.com>
2022-07-28 06:25:09 -0300

    uc

--- src/main/java/com/ap/portfoil/PortfoilBracciale/controllers/UserController.java
+++ src/main/java/com/ap/portfoil/PortfoilBracciale/controllers/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/MyUsers")
-//@CrossOrigin("http://localhost:4200")
+@CrossOrigin("http://localhost:4200")
 public class UserController {
     
     @Autowired
