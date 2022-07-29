9076c53ec2788d3ea743cba5c1c6f602573a5505
Marcos Hernan Bracciale Sauro <marcoshernanbracciale@gmail.com>
2022-07-28 23:10:56 -0300

    Update PortfoilBraccialeApplication.java

--- src/main/java/com/ap/portfoil/PortfoilBracciale/PortfoilBraccialeApplication.java
+++ src/main/java/com/ap/portfoil/PortfoilBracciale/PortfoilBraccialeApplication.java
@@ -2,6 +2,9 @@ package com.ap.portfoil.PortfoilBracciale;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class PortfoilBraccialeApplication {
@@ -9,5 +12,13 @@ public class PortfoilBraccialeApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PortfoilBraccialeApplication.class, args);
 	}
+        
+        @Configuration
+        public class WebConfiguration implements WebMvcConfigurer {
+                    @Override
+                    public void addCorsMappings(CorsRegistry registry){
+            registry.addMapping("/**").allowedMethods("GET","PUT","POST","DELETE");
+        }
+                }
 
 }
